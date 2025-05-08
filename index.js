66cdb48a55405dd3c31f55305c422d2ff9fde150
jamel <niero570@gmail.com>
2025-05-07 21:39:25 -0400

    first push

---
+++ index.js
@@ -0,0 +1,35 @@
+var a
+var b
+
+
+
+
+function sumReturner(a,b){
+
+    var diff = (a + b);
+    console.log(diff);  
+
+}
+sumReturner(54,13);
+
+function diffReturner(a,b){
+
+    var diff = (a - b);
+    console.log(diff);  
+
+}
+diffReturner(54,13);
+function xReturner(a,b){
+
+    var diff = (a * b);
+    console.log(diff);  
+
+}
+xReturner(52,13);
+function divReturner(a,b){
+
+    var diff = (a / b);
+    console.log(diff);  
+
+}
+divReturner(54,13);
